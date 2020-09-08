fa96ac5090520b1e6477262cfe455b946e47296b
Hiromichi Matsui <evergreen.0911@gmail.com>
2020-09-08 11:27:55 +0000

    [Add]admin,productページ作成

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -2,4 +2,21 @@ class Admin::ProductsController < ApplicationController
 	def new
 		@product = Product.new
 	end
+	def create
+		@product = Product.new(product_params)
+		if @product.save!
+			redirect_to admin_product_path(@product)
+		else
+			reder "new"
+		end
+	end
+	def show
+		@product = Product.find(params[:id])
+	end
+
+	private
+
+	def product_params
+		params.require(:product).permit(:genre_id, :name, :description, :price, :sales_status, :image)
+	end
 end
